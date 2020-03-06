85c77c95e71b987fad62030fc3d43e852e44e64b
Michael Labuschke <michaellabuschke@Michaels-MBP.fritz.box>
2020-03-06 15:30:50 +0100

    first commit

---
+++ dotbackup.php
@@ -0,0 +1,53 @@
+<?php
+include("vendor/autoload.php");
+
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+Sentry\init(['dsn' => getenv("SENTRY_DSN") ]);
+$dotenv->required('BASE_DIR')->notempty();
+$dotenv->required('DOTENV_DIR')->notempty();
+$dotenv->required('BACKUP_SUBDIR');
+if (substr($_ENV["BACKUP_SUBDIR"], -1)!="/" && strlen($_ENV["BACKUP_SUBDIR"])!=0) {
+    $_ENV["BACKUP_SUBDIR"].="/";
+}
+if (substr($_ENV["BASE_DIR"], -1)!="/") {
+    $_ENV["BASE_DIR"].="/";
+}
+if (substr($_ENV["DOTENV_DIR"], -1)!="/") {
+    $_ENV["DOTENV_DIR"].="/";
+}
+$files = explode(',', $_ENV["BACKUP_FILES"] ?? ".env");
+$verbose = getenv("VERBOSE");
+if ($verbose === "false") {
+    $verbose = false;
+}
+if ($verbose) {
+    echo "Backing up following files:\n\t".implode("\n\t", $files)."\n";
+}
+
+$it = new RecursiveDirectoryIterator($_ENV["BASE_DIR"]);
+foreach (new RecursiveIteratorIterator($it) as $file) {
+    if (substr($file, 0, strlen($_ENV["DOTENV_DIR"]))!= $_ENV["DOTENV_DIR"]) {
+        if (in_array(basename($file), $files)) {
+            $source = $file;
+            $file = substr($file, strlen($_ENV["BASE_DIR"]));
+            if ($verbose) {
+                echo "found ".basename($file). " in ".dirname($file)."\n";
+            }
+            $backup_dir = $_ENV["DOTENV_DIR"].dirname($file)."/".$_ENV["BACKUP_SUBDIR"];
+            if (!file_exists($backup_dir)) {
+                mkdir($backup_dir, 0755, true);
+            }
+            $dest = $backup_dir.basename($file);
+            if ($verbose) {
+                echo "\tbacking up ".$source."\n\tto ".$dest. "\n";
+            }
+            copy($source, $dest);
+        }
+    }
+}
+chdir($_ENV["DOTENV_DIR"]);
+exec('git add .');
+exec('git commit -m "'.date("y-m-d").'"');
+exec('git push');
